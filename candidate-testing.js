348fa9d50c32348762e970e2ca71eec0bcad6672
Ilyane Fox <>
2021-06-04 03:14:03 +0000

    Completed part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,10 +4,12 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName;
+candidateName = ""
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -16,17 +18,34 @@ let candidateAnswers;
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
+const input = require("readline-sync");
+
+let candidateName = input.question("What is your name? ");
+
+ console.log("Hello, " + candidateName); 
+
 }
+ 
+  
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+const input = require("readline-sync");
+
+let candidateAnswer = input.question(question); 
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+if (candidateAnswer === correctAnswer){
+  console.log("Correct!")
+} else {
+  console.log("Incorrect")
+}
 
 
   let grade;
@@ -38,7 +57,9 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+
+ 
+
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
