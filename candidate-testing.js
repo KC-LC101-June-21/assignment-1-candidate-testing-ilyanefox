063f296d630e66404ddf6d29fca12d8f81dfbb9b
ilyane fox <85144115+ilyanefox@users.noreply.github.com>
2021-06-13 21:28:50 +0000

    Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,14 +20,14 @@ function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
 
-const input = require("readline-sync");
+// const input = require("readline-sync");
 
-let candidateName = input.question("What is your name? ");
+// let candidateName = input.question("What is your name? ");
 
-console.log("Hello, " + candidateName); 
+// console.log("Hello, " + candidateName); 
 
 }
- 
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
@@ -36,37 +36,60 @@ function askQuestion() {
 
 // candidateAnswer = input.question(question); 
 
+//  for (let i = 0; i < questions.length; i++){
+//  candidateAnswers[i] = input.question(questions[i]); 
+//  }
 for (let i = 0; i < questions.length; i++){
- candidateAnswers[i] = input.question(questions[i]); 
-
- }
+  // candidateAnswers[i] = input.question([i + 1] + ")" + " " +  questions[i]);
+  candidateAnswers[i] = input.question(`${[i + 1]}) ${questions[i]}`)
+  console.log(`Your Answer: ${candidateAnswers[i]}\nCorrect answer: ${correctAnswers[i]}`);
+  console.log("\n");
+  // console.log(`Your Answer: ${candidateAnswers[i]}`);
+  // console.log(`Correct Answer: ${correctAnswers[i]}`);
+  // console.log("\n");
 }
-function gradeQuiz(candidateAnswers) {
+
+}
+function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
-// if (candidateAnswer === correctAnswer){
-//   console.log("Correct!")
-// } else {
-//   console.log("Incorrect")
-// }
-console.log(`Candidate's responses: ${candidateAnswers}. Correct answers: ${correctAnswers}`)
 
-  let grade;
+let correctCount = 0
+for (let i = 0; i < questions.length; i ++) {
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+    correctCount += 1;
+  } 
+}
   
 
+let grade = (correctCount)/(questions.length) * 100;
+
+console.log(`>>> Overall Grade: ${grade}% (${correctCount} out of ${questions.length} responses correct) <<<`);
+
   return grade;
+
 }
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
 
+let candidateName = input.question("Candidate Name: ");
  
 
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  let finalGrade = gradeQuiz(this.candidateAnswers);
+
+
+ if (finalGrade >= 80){
+    console.log(`>>> Status: PASSED <<<`)
+      } else {
+        console.log(">>> Status: FAILED <<<")
+    }
 }
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
